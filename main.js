f18d658be5c989aeb7990e761d44c6b2a6934ed8
fallch0222 <56762890+fallch0222@users.noreply.github.com>
2022-05-22 00:17:20 +0900

    real real last

--- main.js
+++ main.js
@@ -2,12 +2,6 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
 
-const sleep = ((delay) => {
-    var start = new Date().getTime();
-    while (new Date().getTime() < start + delay);
-});
- 
-
 
 const zerroAdder = (num) => {
     if (num < 10) {
@@ -18,11 +12,11 @@ const zerroAdder = (num) => {
     }
 }
 
-var fontBase = canvas.width,                   // selected default width for canvas
+    let fontBase = canvas.width,                   // selected default width for canvas
     fontSize = 20;                     
-    var size = 0;
+    let size = 0;
 function getFont() {
-    var ratio = fontSize / fontBase;   // calc ratio
+    let ratio = fontSize / fontBase;   // calc ratio
     size = canvas.width * ratio;   // get font size based on current width
     return (size|0) + 'px sans-serif'; // set font
 }
@@ -33,18 +27,20 @@ const currentTime = (() => {
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
+
     minutes = zerroAdder(minutes);
     seconds = zerroAdder(seconds);
-    let time = `${hours}:${minutes}:${seconds}`;
-    ctx.font = getFont();
 
+    let time = `${hours}:${minutes}:${seconds}`;
+
+    ctx.font = getFont();
     ctx.fillText(time, canvas.width / 2 - ctx.measureText(time).width /2, canvas.height / 2);
     return;
 });
 
 const updateSize = () => {
     canvas.width = screen.width;
-    canvas.height = screen.height - 50;
+    canvas.height = screen.height;
     
 }
 
