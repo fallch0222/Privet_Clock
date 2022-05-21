5ba1b341ba78b08fd580a619ad64fb67fcbb9fe9
fallch0222 <56762890+fallch0222@users.noreply.github.com>
2022-05-21 22:03:29 +0900

    size loading

--- main.js
+++ main.js
@@ -2,40 +2,53 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 ctx.font = "30px Arial";
 
+
+
 const sleep = ((delay) => {
     var start = new Date().getTime();
     while (new Date().getTime() < start + delay);
 });
  
-const Clear = (async () =>{
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
 
+
+const zerroAdder = (num) => {
+    if (num < 10) {
+        return "0" + num;
+    }
+    else{
+        return num;
+    }
+}
+
+const currentTime = (() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    minutes = zerroAdder(minutes);
+    seconds = zerroAdder(seconds);
+    let time = `${hours}:${minutes}:${seconds}`;
+    ctx.fillText(time.toString(), 0, 0 , canvas.width);
+    console.log("filling compleated" + time.toString());
     return;
 });
 
-//출처: https://multifrontgarden.tistory.com/157 [우리집앞마당]
-
-const currentTime = (async () => {
-    var date = new Date();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
-    var time = `${hours}:${minutes}:${seconds}`;
-    ctx.fillText(time, 100, 200);
+const updateSize = () => {
+    canvas.width = screen.width - 100;
+    canvas.height = screen.height - 100;
     
-    return;
-});
-
+}
 
 const updateText = () => {
     setTimeout(()=>{
       //currentTime 안에서 ctx.TextClear()해도 좋을듯
-      Clear();
+      updateSize();
       currentTime();
       updateText();
     }, 1000);
-  };
-  updateText();
+};
+updateText();
 
 
 
