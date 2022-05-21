105ee31f4a834c2df026a89589d90f626b56380e
fallch0222 <56762890+fallch0222@users.noreply.github.com>
2022-05-21 20:17:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,31 +2,38 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 ctx.font = "30px Arial";
 
-function sleep (delay) {
+const sleep = ((delay) => {
     var start = new Date().getTime();
     while (new Date().getTime() < start + delay);
-}
+});
  
+const TextClear = (async () =>{
+    ctx.TextClear();
+    return;
+});
 
 //출처: https://multifrontgarden.tistory.com/157 [우리집앞마당]
 
-async function currentTime() {
+const currentTime = (async () => {
     var date = new Date();
     var hours = date.getHours();
     var minutes = date.getMinutes();
     var seconds = date.getSeconds();
     var time = `${hours}:${minutes}:${seconds}`;
-    ctx.fillText(time, 10, 50);
+    ctx.fillText(time, 100, 200);
     
     return;
-}
+});
 
 
-while(true) {
+while(true){
     currentTime();
-    //await sleep(1000);
+
+    await sleep(1000);
+    setTimeout(() => {
+       await TextClear();
+    }, 1000);
 }
- 
 
 
 
